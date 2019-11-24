aabe7376795f3562fb092be37c3737c57057ed0d
CARLOS CESAR DA FERRAZ JUNIOR <carloscesardaferrazjunior@CARLOSs-MacBook-Pro.local>
2019-11-23 23:07:52 -0500

    verified final version

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const Parcel = class {
         this.trackingNumber = trackingNumber;
         this.status = status;
         this.destination = destination;
-        this.weight = weight;
+        this.weight = parseFloat(weight);
         this.express = express;
     }
 }
+
+//creating an array to store the parcel objects
+var parcels = [];
 
 //declare constants
 const NEW_PARCEL_STATUS = "Processing";
@@ -15,10 +18,6 @@ const SHIPPING_FLAT_RATE = 5;
 const SHIPPING_EXPRESS_ADD = 10;
 const SHIPPING_PER_WEIGHT = 0.05;
 const TAX = 0.13;
-
-//creating an array to store the parcel objects
-var parcels = [
-];
 
 //populate parcel arrays with initial parcels
 var parcelStatus = ["Processing","Shipped","In Transit","Delivered"];
@@ -49,7 +48,8 @@ console.log("parcels=",parcels);
         var destination = parcelDestinationSelection.value;
         console.log("destination=",destination);
         //validate weight input as a number, even if is a number input type
-        var weight = validNumber(parcelWeightInput.value) ? parcelWeightInput.value : null;
+        //the validation is made by a function
+        var weight = validNumberAboveZero(parcelWeightInput.value) ? parcelWeightInput.value : null;
         //if number is not valid, respond to user with field and button changes
         if(!weight){
             parcelWeightInput.value="0";
@@ -73,28 +73,39 @@ console.log("parcels=",parcels);
         console.log("weight=",weight);
 
         //if weight is valid, proceed
+
+        //collect if parcel is express or not
+        //if checked, the expression below returns true
+        //if not, return flase
         var express = parcelExpressCheckbox.checked;
         console.log("express = ", express);
 
         //create another unique tracking number
-        var newTrackingNumber = generateUniqueTrackingNumber();
+        var newTrackingNumber = generateUniqueTrackingNumber();//this function users the generate
         console.log("new tracking number = ", newTrackingNumber);
 
         //create new instance of Parcel and push to parcels array
         parcels.push( new Parcel( newTrackingNumber, NEW_PARCEL_STATUS, destination, weight, express ));
         console.log(parcels);
 
+        //clear create parcel forms and display updated list of parcels
         clearCreateParcels();
         displayParcels();
 
     } // end of function createParcel()
 
+/*
+    processParcel()
+    Purpose: process a parcel, change its status and give information about price
+    Parameters: get from page form
+    Returns: none
+*/
     function processParcel(){
         
-        //get the value from textbox
+        //gets the value from textbox
         var trackingNumberElement = document.getElementById("tbTrackingNumber");
         var trackingNumber = trackingNumberElement.value;
-        //get value from dropdown
+        //gets status from dropdown
         var trackingStatusSelection = document.getElementById("processParcel_Status");
         var trackingStatus = trackingStatusSelection.value;
 
@@ -105,10 +116,11 @@ console.log("parcels=",parcels);
             return false;
         }
 
+        //search the parcel that matches the code inputed by user
+        //if found, returns the real object of the parcel inside the array
         var parcelFound = parcels.find((parcel)=>parcel.trackingNumber.toLowerCase()===trackingNumber.toLowerCase());
-
         console.log("parcel found=",parcelFound);
-
+        //if no parcel is found, display an error to user
         if(parcelFound===undefined){
             throwProcessParcelErrorToUser("Tracking Number Not Found!");
             return false;
@@ -143,6 +155,7 @@ console.log("parcels=",parcels);
         
         alert(alertMessage);
 
+        //after 2 seconds, erase the message to user in the form
         setTimeout(()=>{
             document.getElementById("processParcelMessage").innerHTML="";
         },2000)
@@ -151,8 +164,13 @@ console.log("parcels=",parcels);
         clearProcessParcels();
 
     } // end of function processParcel()
-
-    function validNumber(inputNumber){
+/*
+    validNumberAboveZero()
+    Purpose: verify if the number is valid, is a number, and is greater then zero
+    Parameters: number in input
+    Returns: true if number is valid, false if invalid
+*/
+    function validNumberAboveZero(inputNumber){
         inputNumber = inputNumber.replace(/\s+/,"");
         inputNumber = inputNumber.replace(/\,+/,".");
         if(isNaN(parseFloat(+inputNumber))){
@@ -183,6 +201,12 @@ console.log("parcels=",parcels);
         return trackingNumber;
     } // end generateTrackingNumber
 
+/*
+    generateUniqueTrackingNumber()
+    Purpose: Helper function - uses generateTrackingNumber to generate a unique tracking number in parcels array
+    Parameters: none
+    Returns: string
+*/
     function generateUniqueTrackingNumber(){
         //generate random tracking numbers
         //until it is one that does not exist
@@ -192,8 +216,14 @@ console.log("parcels=",parcels);
             newTrackingNumber = generateTrackingNumber();
         } while( parcels.find( ({ trackingNumber })=>trackingNumber===newTrackingNumber) );
         return newTrackingNumber;
-    }
+    } //end of generateUniqueTrackingNumber
 
+/*
+    clearCreateParcels()
+    Purpose: Helper function - clear create parcels form
+    Parameters: none
+    Returns: none
+*/
     function clearCreateParcels(){
         var parcelDestinationSelection = document.getElementById("createParcel_Destination");
         var parcelWeightInput = document.getElementById("createParcel_Weight");
@@ -202,9 +232,14 @@ console.log("parcels=",parcels);
         parcelDestinationSelection.selectedIndex = 0;
         parcelWeightInput.value = 0;
         parcelExpressCheckbox.checked = false;
-
     }
 
+/*
+    clearProcessParcels()
+    Purpose: Helper function - clear process parcels form
+    Parameters: none
+    Returns: none
+*/
     function clearProcessParcels(){
         let trackingNumberElement = document.getElementById("tbTrackingNumber");
         let trackingStatusSelection = document.getElementById("processParcel_Status");
@@ -214,6 +249,12 @@ console.log("parcels=",parcels);
 
     }
 
+/*
+    displayParcels()
+    Purpose: Helper function - update parcels list with attributes at the page designated area
+    Parameters: none
+    Returns: none
+*/
     function displayParcels(){
         //declare and clear the output are for parcels
         var displayParcelOutput = document.getElementById("displayParcelOutput");
@@ -225,15 +266,16 @@ console.log("parcels=",parcels);
         //verify the value of the filter
         var displayParcelsFilter = document.getElementById("displayParcelsFilter");
         var statusFilter = displayParcelsFilter.value;
+        
         console.log("statusFilter = ", statusFilter);
-
         console.log("parcels before filtering:",parcels);
         
+        //generate a filtered version of the parcels array if different from All
+        //if controlBox is "all" just clone the parcels array
         var parcelsFiltered = statusFilter=="All" ? parcels.slice(0) : parcels.filter((parcel)=>parcel.status===statusFilter);
-
         console.log("parcelsFiltered:",parcelsFiltered);
 
-        //build parcels rows
+        //build parcels rows and append to parcels container (the table)
         var parcelsContainer = document.getElementById("parcelsContainer");
         parcelsFiltered.forEach((parcel)=>{
             let row = buildParcelsListRowMarkup(parcel);
@@ -241,6 +283,12 @@ console.log("parcels=",parcels);
         });
     }
 
+/*
+    buildParcelsListHeaderMarkup()
+    Purpose: Helper function - builds the header of the parcels table
+    Parameters: none
+    Returns: html element of the parcels container and table header
+*/
     function buildParcelsListHeaderMarkup(){
         var tableContainer = document.createElement("div");
         tableContainer.id = "parcelsContainer";
@@ -279,6 +327,12 @@ console.log("parcels=",parcels);
         return tableContainer;
     }
 
+/*
+    buildParcelsListRowMarkup()
+    Purpose: Helper function - builds a row with the parcel information
+    Parameters: parcel object
+    Returns: html element of a row with the parcel inputed
+*/
     function buildParcelsListRowMarkup(parcel){
         var row = document.createElement("div");
         row.className = "parcelsRow";
@@ -289,6 +343,7 @@ console.log("parcels=",parcels);
 
         var statusColumn = document.createElement("div");
         statusColumn.className = "parcelsStatus parcelsColumn";
+        //attribute a different class to the status column according to the status
         switch(parcel.status){
             
             case "Processing":
@@ -330,6 +385,12 @@ console.log("parcels=",parcels);
         return row;
     }
 
+/*
+    textInputValidated()
+    Purpose: Helper function - validate text input for tracking number
+    Parameters: text inputed 
+    Returns: true if text is valid, false if is invalid
+*/
     function textInputValidated(textInput){
         //strip spaces
         textInput = textInput.replace(/\s+/g,"");
@@ -344,6 +405,12 @@ console.log("parcels=",parcels);
         return true;
     }
 
+/*
+    throwProcessParcelErrorToUser()
+    Purpose: Helper function - throw process parcel error to user
+    Parameters: message to go to user 
+    Returns: none
+*/
     function throwProcessParcelErrorToUser(message){
 
         let processParcelButton = document.getElementById("processParcelButton");
@@ -360,6 +427,7 @@ console.log("parcels=",parcels);
             trackingStatusSelection.style.color = "white";
             trackingStatusSelection.disabled = true;
             console.log("tracking number had not a valid value");
+            //after 2 seconds the message disapears
             setTimeout(()=>{
                 processParcelButton.style.backgroundColor = "white";
                 processParcelButton.style.color = "black";
